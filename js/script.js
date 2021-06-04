9fda510d636a0f4d4b8c3a25cae50bbc4856be56
Alexandr <povar.alexandr1901@gmail.com>
2021-06-04 13:59:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// console.log(1);
+// alert('hello!');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "17");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Как вас зовут?', '');
+// answers[1] = prompt('Какая ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+// console.log(typeof(answers));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Hello, ${user}`);
+
+
+
