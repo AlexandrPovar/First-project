78a3ff472e2dc8b566b19fe29c179a599cbf49d2
Alexandr <povar.alexandr1901@gmail.com>
2021-06-04 14:12:14 +0300

    3

--- js/script.js
+++ js/script.js
@@ -9,15 +9,16 @@
 // const answer = prompt("Вам есть 18?", "17");
 // console.log(answer);
 
-// const answers = [];
+const answers = [];
 
-// answers[0] = prompt('Как вас зовут?', '');
-// answers[1] = prompt('Какая ваша фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
+answers[0] = prompt('Как вас зовут?', '');
+answers[1] = prompt('Какая ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
 
 // document.write(answers);
 
 // console.log(typeof(answers));
+
 
 const category = 'toys';
 
@@ -26,6 +27,11 @@ console.log(`https://someurl.com/${category}/5`);
 const user = "Ivan";
 
 alert(`Hello, ${user}`);
+
+
+
+console.log('arr' + " - object");
+console.log(4 + '5');
 
 
 
