7bc2b7a53451e4f79f8c3bd669c89db0a055e922
AlexandrPovar <83906186+AlexandrPovar@users.noreply.github.com>
2021-06-13 14:37:25 +0300

    last add arrows&psevdo0arrows

---
+++ js/array&pseudo-array.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const arr = [1, 2, 3, 6, 8];
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+const str = prompt("", "");
+const products = str.split(", ");
+console.log(products);
